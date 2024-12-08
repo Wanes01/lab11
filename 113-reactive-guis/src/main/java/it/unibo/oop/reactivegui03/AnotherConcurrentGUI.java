62b57ee822f589c4aeb6da033eba87f4f3316ec8
Wanes01 <emir.wanes.aouioua@gmail.com>
2024-12-08 23:03:53 +0100

    113: reactivegui03 completed

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -2,10 +2,7 @@ package it.unibo.oop.reactivegui03;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.TimeUnit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,4 +15,84 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class AnotherConcurrentGUI extends JFrame {
+    private static final long serialVersionUID = 2L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private static final int COUNTDOWN = 1000 * 10;
+    private final JLabel display = new JLabel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+
+    public AnotherConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+
+        stop.addActionListener(e -> stopCounter(agent));
+        up.addActionListener(e -> agent.countUp());
+        down.addActionListener(e -> agent.countDown());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(COUNTDOWN);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                stopCounter(agent);
+            }
+        }).start();
+    }
+
+    private void stopCounter(final Agent agent) {
+        agent.stopCounting();
+        this.stop.setEnabled(false);
+        this.up.setEnabled(false);
+        this.down.setEnabled(false);
+    }
+
+    private class Agent implements Runnable {
+
+        private volatile boolean up = true;
+        private volatile boolean stop;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> AnotherConcurrentGUI.this.display.setText(nextText));
+                    this.counter = up ? this.counter + 1 : this.counter - 1;
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        public void countUp() {
+            this.up = true;
+        }
+
+        public void countDown() {
+            this.up = false;
+        }
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+    }
 }
